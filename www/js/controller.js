f161dc72906255297261f670c381d7697a6a2bff
Kyoungrok Jang <kyoungrok.jang@gmail.com>
2015-09-12 23:40:02 +0900

    Part 1

---
+++ www/js/controller.js
@@ -0,0 +1,34 @@
+/**
+ *  Module
+ *
+ * Description
+ */
+angular.module('starter.controllers', ['firebase'])
+
+.controller('HomeCtrl', ['$scope', '$firebaseAuth', '$state', function($scope, $firebaseAuth, $state) {
+    $scope.login = {};
+    var firebaseObj = new Firebase("https://my-bucket-list.firebaseio.com");
+    var loginObj = $firebaseAuth(firebaseObj);
+
+    $scope.signin = function() {
+        var username = $scope.login.username;
+        var password = $scope.login.password;
+
+        loginObj.$authWithPassword({
+            email: username,
+            password: password
+        })
+        .then(function(user) {
+            // Success callback
+            console.log('Authentication successful');
+            $state.go('userHome');
+        }, function(error) {
+            // Failure callback
+            console.log('Authentication failure');
+        })
+    }
+}])
+
+.controller('UserHomeCtrl', ['$scope', function($scope) {
+
+}])
